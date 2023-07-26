2e9f99d5080aa52cc49f9d69c4b59f2add226672
KiruthickK <kiruthick101@gmail.com>
2023-07-26 23:09:57 +0530

    Basic running state ready

--- Main.java
+++ Main.java
@@ -3,15 +3,17 @@ import TicTacToe.TicTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        TicTacToe game = new TicTacToe();
         Scanner sc = new Scanner(System.in);
+        System.out.print("Welcome to Tic Tac Toe\nWhat you are choosing 'x' or 'o'?(Type x or o any other input will consider to be default settings):");
+        char option = sc.next().charAt(0);
+        TicTacToe game = new TicTacToe(option);
         while (true) {
-            System.out.println("Welcome to Tic Tac Toe");
-            game.StartGame();
+            game.StartGame(sc);
             System.out.print("Do You Want to play again?\n1 for yes, any other number to exit\nYour Opinion:");
-            if (sc.nextInt() != 1) {
+            // if (sc.nextInt() != 1) {
+                game.initiateBoard();
                 break;
-            }
+            // }
         }
         sc.close();
     }
--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -1,15 +1,35 @@
 package TicTacToe;
 
+import java.util.*;
+
 public class TicTacToe {
     private static final int fixed_size = 3;
+    private char player, computer;
+    private int steps;
     char board[][];
 
     public TicTacToe() {
         board = new char[fixed_size][fixed_size];
+        steps = 0;
+        player = 'x';
+        computer = 'o';
         initiateBoard();
     }
 
-    private void initiateBoard() {
+    public TicTacToe(char option) {
+        board = new char[fixed_size][fixed_size];
+        steps = 0;
+        if (option == 'x') {
+            player = 'x';
+            computer = 'o';
+        } else {
+            computer = 'x';
+            player = 'o';
+        }
+        initiateBoard();
+    }
+
+    public void initiateBoard() {
         for (int i = 0; i < fixed_size; i++) {
             for (int j = 0; j < fixed_size; j++) {
                 board[i][j] = ' ';
@@ -17,21 +37,51 @@ public class TicTacToe {
         }
     }
 
+    private boolean isIndexValid(int i, int j) {
+        return (i < fixed_size && j < fixed_size && i >= 0 && j >= 0);
+    }
+
+    private boolean isIndexFree(int i, int j) {
+        return board[i][j] == ' ';
+    }
+
+    private boolean insertBoard(int i, int j) {
+        if (isIndexValid(i, j) && isIndexFree(i, j)) {
+            board[i][j] = player;
+            return true;
+        }
+        System.out.println("The position has filled already or invalid index, try another position!");
+        return false;
+    }
+
     private void printBoard() {
         System.out.println("Tic Tac Toe now:");
         for (int i = 0; i < fixed_size; i++) {
             for (int j = 0; j < fixed_size; j++) {
-                System.out.print(board[i][j]+" ");
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
     }
 
-    public void StartGame() {
+    /**
+     * Main function to play the game
+     * this function will call the helper functions if needed
+     */
+    public void StartGame(Scanner sc) {
         boolean GameOver = false;
-        while(GameOver){
+        while (!GameOver) {
+            if (steps == 9) {
+                GameOver = true;
+                return;
+            }
+            System.out.print("Enter position to insert 'x'(x , y seperated by spaces):");
+            int i = sc.nextInt(), j = sc.nextInt();
+            if (!insertBoard(i, j)) {
+                continue;
+            }
             printBoard();
-            
         }
+        // sc.close();
     }
 }
