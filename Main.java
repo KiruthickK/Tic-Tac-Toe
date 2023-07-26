7cb6b32d787293f1f02228e9a3466fc5f43a3c6a
KiruthickK <kiruthick101@gmail.com>
2023-07-26 22:31:18 +0530

    Initiation completed

--- Main.java
+++ Main.java
@@ -1,4 +1,18 @@
+import java.util.*;
 import TicTacToe.TicTacToe;
+
 public class Main {
-    TicTacToe game = new TicTacToe();
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Welcome to Tic Tac Toe");
+            game.StartGame();
+            System.out.print("Do You Want to play again?\n1 for yes, any other number to exit\nYour Opinion:");
+            if (sc.nextInt() != 1) {
+                break;
+            }
+        }
+        sc.close();
+    }
 }
--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -1,7 +1,37 @@
 package TicTacToe;
 
 public class TicTacToe {
-    public TicTacToe(){
-        
+    private static final int fixed_size = 3;
+    char board[][];
+
+    public TicTacToe() {
+        board = new char[fixed_size][fixed_size];
+        initiateBoard();
+    }
+
+    private void initiateBoard() {
+        for (int i = 0; i < fixed_size; i++) {
+            for (int j = 0; j < fixed_size; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    private void printBoard() {
+        System.out.println("Tic Tac Toe now:");
+        for (int i = 0; i < fixed_size; i++) {
+            for (int j = 0; j < fixed_size; j++) {
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void StartGame() {
+        boolean GameOver = false;
+        while(GameOver){
+            printBoard();
+            
+        }
     }
 }
