b2fc773ceb0d0c870c8230c5c2ba16ee1eb41f06
KiruthickK <kiruthick101@gmail.com>
2023-07-27 14:05:38 +0530

    Some minor bugs fixed

--- Main.java
+++ Main.java
@@ -4,16 +4,17 @@ import TicTacToe.TicTacToe;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Welcome to Tic Tac Toe\nWhat you are choosing 'x' or 'o'?(Type x or o any other input will consider to be default settings):");
-        char option = sc.next().charAt(0);
-        TicTacToe game = new TicTacToe(option);
         while (true) {
+            System.out.print(
+                    "Welcome to Tic Tac Toe\nWhat you are choosing 'x' or 'o'?(Type x or o any other input will consider to be default settings):");
+            char option = sc.next().charAt(0);
+            TicTacToe game = new TicTacToe(option);
             game.StartGame(sc);
             System.out.print("Do You Want to play again?\n1 for yes, any other number to exit\nYour Opinion:");
             if (sc.nextInt() != 1) {
-                game.initiateBoard();
                 break;
             }
+            game.initiateBoard();
         }
         sc.close();
     }
